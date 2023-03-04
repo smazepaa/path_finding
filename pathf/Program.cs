8adc87b2ef38e9cee135b18df2f183983717a4d7
smazepaa <smazepa@kse.ord.ua>
2023-03-04 13:54:50 +0200

    a* algorithm with geometrical distance

--- pathf/Program.cs
+++ pathf/Program.cs
@@ -1,20 +1,15 @@
 ﻿using pathf;
 
-// generating the map
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
     Height = 35,
     Width = 90,
-    Seed = 10,
-    Noise = .1f,
-    AddTraffic = true,
-    TrafficSeed = 1234
+    Seed = 16
 });
 
 var startPoint = new Point(0, 0);
 var endPoint = new Point(16, 30);
 
-List<float> times = new List<float>();
 string[,] map = generator.Generate();
 var path = GetShortestPath(map, startPoint, endPoint);
 new MapPrinter().Print(map, path);
@@ -22,25 +17,20 @@ new MapPrinter().Print(map, path);
 
 List<Point> GetShortestPath(string[,] map, Point start, Point goal)
 {
-    // creating list of points of the path
     List<Point> shortestPath = new List<Point>();
     shortestPath.Add(start);
     
     var distances = new Dictionary<Point, int>(); // dict with distances between points (weight of each edge)
     var origins = new Dictionary<Point, Point>(); // create to find later its neighbours (from which we go)
-    var velocities = new Dictionary<Point, float>();
-    var time = new Dictionary<Point, float>();
     
-
     var frontier = new PriorityQueue<Point, int>(); 
     frontier.Enqueue(start, 0);
-    origins[start] = start; // to remember point (0;0)
+    origins[start] = start;
     distances[start] = 0;
 
     Point current = default;
     while (frontier.Count != 0)
     {
-        // ? verification doesn't work
         current = frontier.Dequeue();
         if (current.Equals(goal))
         {
@@ -49,15 +39,11 @@ List<Point> GetShortestPath(string[,] map, Point start, Point goal)
         var neighbours = GetNeighbours(map, current);
         foreach (var next in neighbours)
         {
-            var n = Convert.ToInt32(map[next.Column, next.Row]);
-            
-            var newDistance = distances[current] + n;
+            var newDistance = distances[current] + 1;
             if (!origins.ContainsKey(next) || newDistance < distances[next])
             {
-                velocities[next] = 60 - (n - 1) * 6;
-                times.Add(1/velocities[next]);
                 distances[next] = newDistance;
-                var priority = newDistance;
+                var priority = newDistance + Geometrical(goal, next);
                 frontier.Enqueue(next, priority);
                 origins[next] = current;
             }
@@ -75,21 +61,46 @@ List<Point> GetShortestPath(string[,] map, Point start, Point goal)
     return shortestPath;
 }
 
+int Heuristic(Point goal, Point next)
+{
+    return Math.Abs(goal.Row - next.Row) + Math.Abs(goal.Column - next.Column);
+}
+
+int Geometrical(Point goal, Point next)
+{
+    return (goal.Row - next.Row) ^ 2 + (goal.Column - next.Column) ^ 2;
+}
+
 List<Point> GetNeighbours(string[,] map, Point point)
 {
     List<Point> neighbours = new List<Point>();
     var width = map.GetLength(0);
     var height = map.GetLength(1);
-    
-    if (point.Column - 1 != -1) // if we go left, !=-1 so we don't go outside the map
+    if (point.Column - 1 != -1) // west
     {
-        if (map[point.Column - 1, point.Row] != "█") // if it's not a wall
+        if (map[point.Column - 1, point.Row] != "█") 
         {
             neighbours.Add(new Point(point.Column - 1, point.Row));
         }
     }
 
-    if (point.Row - 1 != -1)
+    if (point.Column - 1 != -1 && point.Row - 1 != -1) // north-west
+    {
+        if (map[point.Column - 1, point.Row - 1 ] != "█") 
+        {
+            neighbours.Add(new Point(point.Column - 1, point.Row - 1));
+        }
+    }
+    
+    if (point.Column - 1 != -1 && point.Row + 1 < height) // south-west
+    {
+        if (map[point.Column - 1, point.Row + 1 ] != "█") 
+        {
+            neighbours.Add(new Point(point.Column - 1, point.Row + 1));
+        }
+    }
+
+    if (point.Row - 1 != -1) // north
     {
         if (map[point.Column, point.Row - 1] != "█")
         {
@@ -97,7 +108,7 @@ List<Point> GetNeighbours(string[,] map, Point point)
         }
     }
 
-    if (point.Column + 1 >= 0 && point.Column + 1 < width && point.Row < height)
+    if (point.Column + 1 >= 0 && point.Column + 1 < width && point.Row < height) // east
     {
         if (map[point.Column + 1, point.Row] != "█")
         {
@@ -105,34 +116,29 @@ List<Point> GetNeighbours(string[,] map, Point point)
         }    
     }
 
-    if (point.Row + 1 >= 0 && point.Row + 1 < height && point.Column < width)
+    if (point.Row + 1 >= 0 && point.Row + 1 < height && point.Column < width) // south
     {
         if (map[point.Column, point.Row + 1] != "█")
         {
             neighbours.Add(new Point(point.Column, point.Row + 1));
         }            
     }
-
-    foreach (var n in neighbours)
+    
+    if (point.Row + 1 < height && point.Column + 1 < width) // south-east
     {
-        if (map[n.Column, n.Row] == "B")
+        if (map[point.Column + 1, point.Row + 1] != "█")
         {
-            Console.WriteLine("B");
+            neighbours.Add(new Point(point.Column + 1, point.Row + 1));
         }
     }
-
+    
+    if (point.Row - 1 != -1 && point.Column + 1 < width) // north-east
+    {
+        if (map[point.Column + 1, point.Row - 1] != "█")
+        {
+            neighbours.Add(new Point(point.Column + 1, point.Row - 1));
+        }
+    }
+    
     return neighbours;
 }
-
-
-float totalTime = times.Sum();
-Console.WriteLine();
-Console.WriteLine("total travel time:" + Math.Round(totalTime, 2) + "hours");
-//Console.WriteLine(map.GetLength(0));
-//Console.WriteLine(map.GetLength(1));
-
-// dimension 0 - width - columns
-// dimension 1 - height - rows
-
-// check movement cost
-// a star and heuristic
